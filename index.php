3fd91eab39d32c67828bfacf4a8da1229d5c1ca3
Opeyemi Rowland <rowlandopeyemi@gmail.com>
2021-08-23 00:24:24 +0100

    Update index.php

--- index.php
+++ index.php
@@ -285,7 +285,7 @@
                 <p>+234 8064 7998 80</p>
               </div>
 
-              <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d253782.7993043786!2d5.502376525301576!3d6.347897633583698!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1040d36279cfacf1%3A0x84aa10c05758dfa0!2sBenin%20City!5e0!3m2!1sen!2sng!4v1629674101452!5m2!1sen!2sng" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy">" frameborder="0" style="border:0; width: 100%; height: 290px;" allowfullscreen></iframe>
+              <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d253782.7993043786!2d5.502376525301576!3d6.347897633583698!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1040d36279cfacf1%3A0x84aa10c05758dfa0!2sBenin%20City!5e0!3m2!1sen!2sng!4v1629674101452!5m2!1sen!2sng" frameborder="0" style="border:0; width: 100%; height: 290px;" allowfullscreen></iframe>
             </div>
 
           </div>
